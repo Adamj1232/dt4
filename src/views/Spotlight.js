c0c9228ea43ee4dad49b509370749184e40603a9
Adam Carpenter <adamcarpenter86@gmail.com>
2023-07-14 10:44:13 -0600

    Updates to spotlight

--- src/views/Spotlight.js
+++ src/views/Spotlight.js
@@ -1,35 +1,51 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 const Spotlight = () => {
   const spotlightRef = useRef(null);
-  let spotlightSize = 'transparent 90px, rgba(0, 0, 0, 1) 120px)';
+  let initSpotlightSize = "transparent 0px, rgba(0, 0, 0, 1) 1px)";
+  let spotlightSize = "transparent 90px, rgba(0, 0, 0, 1) 120px)";
+  let initUpdate = 0;
 
   const updateSpotlight = (e) => {
     if (spotlightRef.current) {
-      spotlightRef.current.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+      spotlightRef.current.style.backgroundImage = `radial-gradient(circle at ${
+        (e.pageX / window.innerWidth) * 100
+      }% ${(e.pageY / window.innerHeight) * 100}%, ${
+        initUpdate < 2 ? initSpotlightSize : spotlightSize
+      }`;
+      initUpdate += 1;
     }
   };
 
   useEffect(() => {
     const handleMouseMove = (e) => updateSpotlight(e);
     const handleMouseUp = (e) => {
-      spotlightSize = 'transparent 90px, rgba(0, 0, 0, 1) 120px)';
+      spotlightSize = "transparent 90px, rgba(0, 0, 0, 1) 120px)";
       updateSpotlight(e);
     };
     const handleMouseDown = (e) => {
-      spotlightSize = 'transparent 110px, rgba(0, 0, 0, 0.9) 140px)';
+      spotlightSize = "transparent 110px, rgba(0, 0, 0, 0.9) 140px)";
       updateSpotlight(e);
     };
 
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mousedown', handleMouseDown);
-    window.addEventListener('mouseup', handleMouseUp);
+    // Create a mock event object
+    const mockEvent = {
+      pageX: window.innerWidth / 2,
+      pageY: window.innerHeight / 2,
+    };
+
+    // Call updateSpotlight with the mock event
+    updateSpotlight(mockEvent);
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mouseup", handleMouseUp);
 
     // Clean up event listeners on unmount
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('mousedown', handleMouseDown);
-      window.removeEventListener('mouseup', handleMouseUp);
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
     };
   }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
 
